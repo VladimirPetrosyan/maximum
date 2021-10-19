f70ed01821b1fbfe2510f6bc15c1b0066b2dc1f7
VladimirPetrosyan <petrosyan412@icloud.com>
2021-10-19 17:28:37 +0300

    Добавьте файлы проекта.

---
+++ maximum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace maximum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c;
+            Console.WriteLine("Введите 3 любых числа, и я скажу какое больше :)");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a < b ||c < b)
+            {
+                Console.WriteLine(b);
+            if (a < c||b < c )
+                {
+                    Console.WriteLine(c);
+            if (c < a || b < a)
+                    {
+                        Console.WriteLine(a)
+                    }
+                }
+            }
+            
+        }
+    }
+}
